f33e5f23aefa4be45de563a9b1180f05ce743826
lloyd-fedora <lloyd.williams1062@gmail.com>
2024-10-17 04:14:57 +1100

    🙃

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -7,11 +7,40 @@ return {
             vim.o.background = "dark" -- or "light" for light mode
             vim.g.gruvbox_material_background = "soft"
             vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-            vim.cmd([[colorscheme gruvbox-material]])
+            vim.cmd([[colorscheme gruvbox]])
         end
     },
     {
-        "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ...
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            -- Default options:
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true,    -- invert background for search, diffs, statuslines and errors
+                contrast = "soft", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+            vim.cmd("colorscheme gruvbox")
+        end,
     },
     {
         "catppuccin/nvim", name = "catppuccin", priority = 1000
