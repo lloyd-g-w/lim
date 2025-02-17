0cc07c90993a125cef1f67d8fbf155d9f849472f
lloyd-fedora <lloyd.williams1062@gmail.com>
2025-02-17 13:27:22 +1100

    test

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,5 +4,8 @@ return {
     "nvim-lua/plenary.nvim",  -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
   },
-  config = true
+  config = function()
+    -- bind leader g to open neogit
+    vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>Neogit<CR>', { noremap = true, silent = true })
+  end,
 }
