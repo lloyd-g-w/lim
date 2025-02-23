e39ecab55e132ebcbc2f8645b43eacef8422f229
lloyd-fedora <lloyd.williams1062@gmail.com>
2025-02-23 16:08:32 +1100

    🙃

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,8 @@ return {
 
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup()
+        require('lualine').setup({
+            options = { theme = 'gruvbox-material' },
+        });
     end
 }
