311423b2544a85e3d1295bb3219125f41ed5f539
Lloyd <lloyd.williams1062@gmail.com>
2025-04-05 16:03:10 +1100

    🙃

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,5 +6,8 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, silent = true, desc='open neotree'})
+  end
 }
