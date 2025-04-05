54ac695475cf44697de91482de0f779f08a9b69c
Lloyd <lloyd.williams1062@gmail.com>
2025-04-06 02:18:19 +1100

    🙃

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,7 @@
 return {
     "lervag/vimtex",
     init = function()
-        vim.cmd("set conceallevel=2")
+        -- vim.cmd("set conceallevel=2")
         vim.g.vimtex_view_method = "zathura"
         vim.g.vimtex_compiler_method = "latexmk"
         vim.g.vimtex_compiler_latexmk = {
