de96ac6335b123d9d670351b2d688189ae87b994
Lloyd <lloyd.williams1062@gmail.com>
2025-04-13 19:20:13 +1000

    🙃

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+        require('mini.files').setup({
+            options = { permanent_delete = false }
+        });
+        require('mini.ai').setup()
+    end
+}
