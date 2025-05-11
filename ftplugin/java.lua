e581e895e97b20ce4b686da3b302e7f9c7cd32f7
Lloyd <lloyd.williams1062@gmail.com>
2025-05-11 23:14:15 +1000

    🙃

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,20 +1,20 @@
-local jdtls = require('jdtls')
-local jdtls_setup = require('jdtls.setup')
-local home = os.getenv('HOME')
+local jdtls = require("jdtls")
+local jdtls_setup = require("jdtls.setup")
+local home = os.getenv("HOME")
 
 -- Get Java 21 executable path from environment variable
-local java_exe = os.getenv('JAVA_21_EXE')
-if not java_exe then
-  -- Fallback if the environment variable isn't set
-  print("Warning: JAVA_21_EXE environment variable not set, using default 'java' command")
-  java_exe = 'java'
-end
+-- local java_exe = os.getenv('JAVA_21_EXE')
+-- if not java_exe then
+--   -- Fallback if the environment variable isn't set
+--   print("Warning: JAVA_21_EXE environment variable not set, using default 'java' command")
+--   java_exe = 'java'
+-- end
 
 -- Find root of project
-local root_markers = { 'gradlew', 'mvnw', '.git', 'pom.xml', 'build.gradle' }
+local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }
 local root_dir = jdtls_setup.find_root(root_markers)
 if root_dir == "" then
-  root_dir = vim.fn.getcwd()
+	root_dir = vim.fn.getcwd()
 end
 
 -- Setup JDTLS config
@@ -25,25 +25,30 @@ local jar_path = vim.fn.glob(plugins_path .. "org.eclipse.equinox.launcher_*.jar
 local lombok_path = jdtls_path .. "/lombok.jar"
 
 local config = {
-  cmd = {
-    java_exe, -- Use the Java 21 executable here
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-javaagent:' .. lombok_path,
-    '-jar', jar_path,
-    '-configuration', config_path,
-    '-data', home .. '/.cache/jdtls-workspace/' .. vim.fn.fnamemodify(root_dir, ":p:h:t"),
-  },
-  root_dir = root_dir,
-  -- Rest of your configuration remains the same
-  -- ...
+	cmd = {
+		java_exe, -- Use the Java 21 executable here
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. lombok_path,
+		"-jar",
+		jar_path,
+		"-configuration",
+		config_path,
+		"-data",
+		home .. "/.cache/jdtls-workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t"),
+	},
+	root_dir = root_dir,
+	-- Rest of your configuration remains the same
+	-- ...
 }
 
 -- Set up the LSP
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,25 +1,20 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local java_exe = os.getenv("JAVA_21_EXE")
-
-        local lspconfig = require('lspconfig')
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'lsp go to definition' })
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'lsp hover' })
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { desc = 'lsp code actions' })
-        require 'lspconfig'.hls.setup {}
-        lspconfig.nil_ls.setup {}
-        lspconfig.ocamllsp.setup {}
-        -- lspconfig.jdtls.setup {
-        --     cmd = { 'jdtls', '--java-executable', java_exe }
-        -- }
-        lspconfig.svelte.setup {}
-        lspconfig.csharp_ls.setup {}
-        lspconfig.ts_ls.setup {}
-        lspconfig.basedpyright.setup {}
-        lspconfig.vimls.setup {}
-        lspconfig.lua_ls.setup {}
-        lspconfig.cmake.setup {}
-        lspconfig.texlab.setup {}
-    end
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "lsp go to definition" })
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "lsp hover" })
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "lsp code actions" })
+		require("lspconfig").hls.setup({})
+		lspconfig.nil_ls.setup({})
+		lspconfig.ocamllsp.setup({})
+		lspconfig.svelte.setup({})
+		lspconfig.csharp_ls.setup({})
+		lspconfig.ts_ls.setup({})
+		lspconfig.basedpyright.setup({})
+		lspconfig.vimls.setup({})
+		lspconfig.lua_ls.setup({})
+		lspconfig.cmake.setup({})
+		lspconfig.texlab.setup({})
+	end,
 }
