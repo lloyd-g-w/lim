95aa316c7dc420eb41cce6ea7d0113a4e8cc685d
Lloyd <lloyd.williams1062@gmail.com>
2025-05-20 17:14:36 +1000

    🙃

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "svelte", "typescript", "javascript" },
             auto_install = true,
             highlight = { enable = true, disable = { "latex" } },
             indent = { enable = true },
