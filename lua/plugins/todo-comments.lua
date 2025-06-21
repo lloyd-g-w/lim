dc0eeaa623064fa5ab0d7a20e810536df8788c6c
Lloyd <lloyd.williams1062@gmail.com>
2025-06-22 02:57:10 +1000

    🙃

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -33,7 +33,7 @@ return {
 			multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
 			multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
 			before = "fg", -- "fg" or "bg" or empty
-			keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+			keyword = "wide_fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 			after = "fg", -- "fg" or "bg" or empty
 			pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
 			comments_only = true, -- uses treesitter to match keywords in comments only
@@ -61,8 +61,7 @@ return {
 			},
 			-- regex that will be used to match keywords.
 			-- don't replace the (KEYWORDS) placeholder
-			pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-			-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+			pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
 		},
 	},
 }
