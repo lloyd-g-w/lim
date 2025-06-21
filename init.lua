5e175690113028b44d7a55adf4eb2dabc5bc27e9
Lloyd <lloyd.williams1062@gmail.com>
2025-06-22 02:37:04 +1000

    🙃

--- init.lua
+++ init.lua
@@ -4,14 +4,14 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 vim.o.winbar = ""
@@ -19,4 +19,6 @@ vim.o.winbar = ""
 -- Files to load
 
 require("vim-options")
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json", -- ~/.local/share/nvim/lazy-lock.json
+})
