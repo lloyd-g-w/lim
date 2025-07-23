27e0fb0f8af1f68a1f220f5fb71546941487d534
Lloyd <lloyd.williams1062@gmail.com>
2025-07-23 19:58:05 +1000

    🙃

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,38 +1,38 @@
 return {
-    "folke/which-key.nvim",
-    dependencies = {
-        { "echasnovski/mini.icons", version = "*" },
-    },
-    opts = {
-        defaults = {},
-        spec = {
-            {
-                mode = { "n", "v" },
-                { "<leader>f", group = "file" },
-                { "<leader>c", group = "code" },
-            },
-        },
-    },
-    event = "VeryLazy",
-    keys = {
-        {
-            "<leader>?",
-            function()
-                require("which-key").show({ global = true })
-            end,
-            desc = "buffer Keymaps (which-key)",
-        },
-        {
-            "<leader>f",
-            group = "file",
-        },
-        {
-            "<leader>fe",
-            function()
-                vim.cmd(":lua MiniFiles.open()")
-                -- vim.cmd("Oil")
-            end,
-            desc = "file explorer",
-        }
-    },
+	"folke/which-key.nvim",
+	dependencies = {
+		{ "echasnovski/mini.icons", version = "*" },
+	},
+	opts = {
+		defaults = {},
+		spec = {
+			{
+				mode = { "n", "v" },
+				{ "<leader>f", group = "file" },
+				{ "<leader>c", group = "code" },
+			},
+		},
+	},
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = true })
+			end,
+			desc = "buffer Keymaps (which-key)",
+		},
+		{
+			"<leader>f",
+			group = "file",
+		},
+		{
+			"<leader>fe",
+			function()
+				vim.cmd(":lua MiniFiles.open()")
+				-- vim.cmd("Oil")
+			end,
+			desc = "file explorer",
+		},
+	},
 }
