0d73e80e00824795123b122af99c014135d63c31
Lloyd <lloyd.williams1062@gmail.com>
2025-07-26 21:48:54 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "neovim flake with config and deps";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {
