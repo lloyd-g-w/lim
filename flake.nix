95b7bc5453b53584fe80a33d93766bd3ed78a4cd
Lloyd <lloyd.williams1062@gmail.com>
2025-07-27 23:17:14 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -11,7 +11,10 @@
     ...
   } @ inputs: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
     deps = with pkgs; [
       # For lazygit plugin
       lazygit
