26c2061d8c6b53470c12441360eb1f92364e7408
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 01:37:31 +1000

    🙃

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,8 +5,8 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "gruvbox-material",
-				-- theme = 'catppuccin',
+				-- theme = "gruvbox-material",
+				theme = 'catppuccin',
 				section_separators = { "" },
 				component_separators = { "" },
 			},
