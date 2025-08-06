b6742c70eade22b70032f1fffb0439f2e0dc2f50
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 01:09:27 +1000

    🙃

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,51 +1,62 @@
 return {
-    {
-        "dracula/vim",
-        name = "dracula",
-        priority = 1000,
-        config = function()
-            vim.o.background = "dark" -- or "light" for light mode
-            vim.g.gruvbox_material_background = "soft"
-            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-            vim.cmd([[colorscheme gruvbox-material]])
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            -- Default options:
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "soft", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-            -- vim.cmd("colorscheme gruvbox")
-        end,
-    },
-    {
-        "catppuccin/nvim", name = "catppuccin", priority = 1000
-    },
-    {
-        "sainnhe/gruvbox-material", priority = 1000, config = true, opts = ...
-    }
+	{
+		"dracula/vim",
+		name = "dracula",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark" -- or "light" for light mode
+			vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+			vim.cmd([[colorscheme gruvbox-material]])
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		-- config = function()
+		--     -- Default options:
+		--     require("gruvbox").setup({
+		--         terminal_colors = true, -- add neovim terminal colors
+		--         undercurl = true,
+		--         underline = true,
+		--         bold = true,
+		--         italic = {
+		--             strings = true,
+		--             emphasis = true,
+		--             comments = true,
+		--             operators = false,
+		--             folds = true,
+		--         },
+		--         strikethrough = true,
+		--         invert_selection = false,
+		--         invert_signs = false,
+		--         invert_tabline = false,
+		--         invert_intend_guides = false,
+		--         inverse = true, -- invert background for search, diffs, statuslines and errors
+		--         contrast = "soft", -- can be "hard", "soft" or empty string
+		--         palette_overrides = {},
+		--         overrides = {},
+		--         dim_inactive = false,
+		--         transparent_mode = false,
+		--     })
+		-- vim.cmd("colorscheme gruvbox")
+		-- end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "frappe", -- latte, frappe, macchiato, mocha
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000,
+		config = true,
+		opts = ...,
+	},
 }
