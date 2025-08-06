1cb02c3d2f0ad1ab70e90d5405fa1599819f3151
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 01:00:44 +1000

    🙃

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -57,6 +57,10 @@ return {
 		local d = ls.dynamic_node
 		local sn = ls.snippet_node
 
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+		vim.lsp.handlers["textDocument/signatureHelp"] =
+			vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
 		-- CUSTOM SNIPPETS
 		local c_type_snips = {
 			s("cheadercomment", {
