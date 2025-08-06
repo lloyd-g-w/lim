0898354658ffe5d07c9024a0dfaea1ade54a0b26
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 01:47:34 +1000

    🙃

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -45,7 +45,7 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
+		priority = 1100,
 		config = function()
 			require("catppuccin").setup({
 				auto_integrations = true,
