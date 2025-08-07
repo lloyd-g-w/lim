47d19f2f0caaafb9b1d0826cab3b4c157491cf00
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 16:56:15 +1000

    🙃

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -4,12 +4,17 @@ return {
 		{ "nvim-tree/nvim-web-devicons", lazy = true },
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>m", require("grapple").toggle)
-		vim.keymap.set("n", "<leader>M", require("grapple").toggle_tags)
+		local grapple = require("grapple")
 
-		-- User command
-		for i = 1, 6, 1 do
-			vim.keymap.set("n", "<leader>" .. i, "<cmd>Grapple select index=" .. i .. "<cr>")
+		-- Toggle current file and tags
+		vim.keymap.set("n", "<leader>m", grapple.toggle, { desc = "Grapple Toggle File" })
+		vim.keymap.set("n", "<leader>M", grapple.toggle_tags, { desc = "Grapple Toggle Tags" })
+
+		-- Select by index (1-6)
+		for i = 1, 6 do
+			vim.keymap.set("n", "<leader>" .. i, function()
+				grapple.select({ index = i })
+			end, { desc = "Grapple Select " .. i })
 		end
 	end,
 }
