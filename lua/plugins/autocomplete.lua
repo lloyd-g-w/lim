ad0b8756d45db37edcbf2931a39a7e2fe702be49
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 15:14:25 +1000

    🙃

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,104 +1,156 @@
 -- return {
---   -- Primary completion engine
---   {
---     "ms-jpq/coq_nvim",
---     branch = "coq",
---     dependencies = {
---       -- Snippet engine support
---       { "ms-jpq/coq.artifacts", branch = "artifacts" },
---       -- LSP completion source
---       { "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
---       -- Latex symbols completion
---       { "onsails/lspkind.nvim" },
---       -- Friendly snippets integration
---       { "rafamadriz/friendly-snippets" },
---       -- LuaSnip loader
---       { "L3MON4D3/LuaSnip" },
---       { "saadparwaiz1/cmp_luasnip" },
---       { "iurimateus/luasnip-latex-snippets.nvim" },
---     },
---     config = function()
---       -- Load VSCode-style snippets
---       require("luasnip.loaders.from_vscode").lazy_load()
---       require("luasnip.loaders.from_lua").load()
+-- 	"hrsh7th/nvim-cmp",
+-- 	dependencies = {
+-- 		"hrsh7th/cmp-cmdline",
+-- 		"hrsh7th/cmp-path",
+-- 		"hrsh7th/cmp-buffer",
+-- 		"hrsh7th/cmp-nvim-lsp",
+-- 		"L3MON4D3/LuaSnip",
+-- 		"saadparwaiz1/cmp_luasnip",
+-- 		"onsails/lspkind.nvim",
+-- 		"rafamadriz/friendly-snippets",
+-- 		"iurimateus/luasnip-latex-snippets.nvim",
+-- 	},
+-- 	config = function()
+-- 		build = "make install_jsregexp"
+-- 		require("luasnip.loaders.from_vscode").lazy_load()
+-- 		local cmp = require("cmp")
+-- 		cmp.setup({
+-- 			snippet = {
+-- 				-- REQUIRED - you must specify a snippet engine
+-- 				expand = function(args)
+-- 					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+-- 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+-- 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+-- 				end,
+-- 			},
+-- 			-- window = {
+-- 			-- completion = cmp.config.window.bordered({
+-- 			-- 	border = "rounded",
+-- 			-- 	winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+-- 			-- }),
+-- 			-- documentation = cmp.config.window.bordered({
+-- 			-- 	border = "rounded",
+-- 			-- }),
+-- 			-- },
+-- 			mapping = cmp.mapping.preset.insert({
+-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+-- 				["<C-Space>"] = cmp.mapping.complete(),
+-- 				["<C-e>"] = cmp.mapping.abort(),
+-- 				["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+-- 				["<Tab>"] = cmp.mapping.select_next_item(),
+-- 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+-- 			}),
+-- 			sources = cmp.config.sources({
+-- 				{ name = "nvim_lsp" },
+-- 				{ name = "luasnip" }, -- For luasnip usersx.
+-- 				{ name = "buffer" },
+-- 				{ name = "path" },
+-- 			}),
+-- 		})
 --
---       -- Coq setup
---       require("coq").setup({
---         auto_start = true,
---         keymap = {
---           -- Navigate completion menu
---           eval_snips = "<C-K>",
---           prev = "<S-Tab>",
---           next = "<Tab>",
---           select = "<CR>",
---         },
---         clients = {
---           -- Enable LSP completion
---           lsp = { enabled = true },
---           -- Add buffer completion
---           buffer = { enabled = true },
---           -- Path completion
---           path = { enabled = true },
---           -- Snippet completions via LuaSnip
---           snippets = { enabled = true, engine = "luasnip" },
---         },
---         icons = {
+-- 		require("luasnip").config.setup({ enable_autosnippets = true })
 --
---         }
---       })
+-- 		require("luasnip-latex-snippets").setup()
 --
---       -- lspkind for symbol annotations
---       local lspkind = require("lspkind")
---       require("coq_3p").register({
---         lspkind = {
---           with_text = true,
---           maxwidth = 35,
---           menu = {
---             buffer = "[Buffer]",
---             lsp = "[LSP]",
---             luasnip = "[LuaSnip]",
---             latex_symbols = "[Latex]",
---             path = "[Path]",
---           },
---         },
---       })
+-- 		local ls = require("luasnip")
+-- 		local s = ls.snippet
+-- 		local t = ls.text_node
+-- 		local i = ls.insert_node
+-- 		local d = ls.dynamic_node
+-- 		local sn = ls.snippet_node
 --
---       -- Custom C/CPP/Java snippets
---       local ls = require("luasnip")
---       local s, t, i, d, sn = ls.snippet, ls.text_node, ls.insert_node, ls.dynamic_node, ls.snippet_node
---       local c_type_snips = {
---         s("cheadercomment", {
---           t("// Lloyd Williams (z5599988) | Written on " .. os.date("%d/%m/%Y ")),
---           t({ "", "// Description: " }),
---         }),
---         s("bigcomment", {
---           t("// " .. string.rep("=", 20) .. " "),
---           i(1),
---           t(" " .. string.rep("=", 20) .. " //"),
---         }),
---       }
---       ls.add_snippets("c", c_type_snips)
---       ls.add_snippets("cpp", c_type_snips)
---       ls.add_snippets("java", { c_type_snips[2] })
+-- 		-- CUSTOM SNIPPETS
+-- 		local c_type_snips = {
+-- 			s("cheadercomment", {
+-- 				t("// Lloyd Williams (z5599988) | Written on " .. os.date("%d/%m/%Y ")),
+-- 				t({ "", "// Description: " }),
+-- 			}),
+-- 			s("bigcomment", {
+-- 				t("// " .. string.rep("=", 20) .. " "),
+-- 				i(1),
+-- 				t(" " .. string.rep("=", 20) .. " //"),
+-- 			}),
+-- 		}
+-- 		ls.add_snippets("c", c_type_snips)
 --
---       -- TeX snippets with mathzone detection
---       local in_mathzone = function()
---         return vim.fn["vimtex#syntax#in_mathzone"]() == 1
---       end
---       ls.add_snippets("tex", {
---         s({ trig = "bf", snippetType = "autosnippet" },
---           d(1, function()
---             if in_mathzone() then
---               return sn(nil, { t("\\mathbf ") })
---             else
---               return sn(nil, { t("\\textbf ") })
---             end
---           end)
---         ),
---         s({ trig = "bb", snippetType = "autosnippet" }, { t("\\mathbb ") }),
---       })
---     end,
---   },
+-- 		ls.add_snippets("cpp", c_type_snips)
+--
+-- 		ls.add_snippets("java", { c_type_snips[2] })
+--
+-- 		local in_mathzone = function()
+-- 			return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+-- 		end
+--
+-- 		ls.add_snippets("tex", {
+-- 			s(
+-- 				{ trig = "bf", snippetType = "autosnippet" },
+-- 				d(1, function()
+-- 					if in_mathzone() then
+-- 						return sn(nil, {
+-- 							t("\\mathbf "),
+-- 						})
+-- 					else
+-- 						return sn(nil, {
+-- 							t("\\textbf "),
+-- 						})
+-- 					end
+-- 				end),
+-- 				{
+-- 					condition = function()
+-- 						return true
+-- 					end,
+-- 				} -- ensures it's always checked
+-- 			),
+-- 			s({ trig = "bb", snippetType = "autosnippet" }, {
+-- 				t("\\mathbb "),
+-- 			}),
+-- 		})
+--
+-- 		require("luasnip.loaders.from_lua").load()
+--
+-- 		-- END CUSTOM SNIPPETS
+--
+-- 		vim.keymap.set({ "i" }, "<C-K>", function()
+-- 			ls.expand()
+-- 		end, { silent = true })
+-- 		vim.keymap.set({ "i", "s" }, "<C-L>", function()
+-- 			ls.jump(1)
+-- 		end, { silent = true })
+-- 		vim.keymap.set({ "i", "s" }, "<C-J>", function()
+-- 			ls.jump(-1)
+-- 		end, { silent = true })
+--
+-- 		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+-- 			if ls.choice_active() then
+-- 				ls.change_choice(1)
+-- 			end
+-- 		end, { silent = true })
+--
+-- 		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+--
+-- 		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+--
+-- 		local lspkind = require("lspkind")
+-- 		cmp.setup({
+-- 			formatting = {
+-- 				format = lspkind.cmp_format({
+-- 					mode = "symbol_text",
+-- 					maxwidth = 35,
+-- 					menu = {
+-- 						buffer = "[Buffer]",
+-- 						nvim_lsp = "[LSP]",
+-- 						luasnip = "[LuaSnip]",
+-- 						nvim_lua = "[Lua]",
+-- 						latex_symbols = "[Latex]",
+-- 						cmdline = "[cmd]",
+-- 						path = "[Path]",
+-- 					},
+-- 				}),
+-- 			},
+-- 		})
+-- 	end,
 -- }
 
 return {
@@ -106,63 +158,98 @@ return {
 		"ms-jpq/coq_nvim",
 		lazy = false,
 		dependencies = {
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-			{ "ms-jpq/coq.thirdparty", branch = "3p" },
+			"ms-jpq/coq.artifacts",
+			"ms-jpq/coq.thirdparty",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
 			"iurimateus/luasnip-latex-snippets.nvim",
+			"onsails/lspkind.nvim",
 		},
 		init = function()
-			local cp = require("catppuccin.palettes").get_palette("frappe")
-
 			vim.g.coq_settings = {
-				auto_start = "shut-up",
+				auto_start = true,
+				notify = false,
+				keymap = { recommended = true },
 				clients = {
-					lsp = { enabled = true },
-					buffer = { enabled = true },
-					path = { enabled = true },
+					lsp = { enabled = true, min_chars = 1 },
 					snippets = { enabled = true, engine = "luasnip" },
+					buffer = { enabled = true, min_chars = 1 },
+					path = { enabled = true, min_chars = 1 },
 				},
 			}
 		end,
 		config = function()
-			-- Snippet loading
+			-- Load VSCode-format snippets & enable autosnippets
 			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").load()
+			require("luasnip").config.setup({ enable_autosnippets = true })
+			require("luasnip-latex-snippets").setup()
 
-			-- Your custom snippets (unchanged)
+			-- Custom C/C++/Java header-comment snippets
 			local ls = require("luasnip")
-			local s, t, i, d, sn = ls.snippet, ls.text_node, ls.insert_node, ls.dynamic_node, ls.snippet_node
+			local s, t, i, sn = ls.snippet, ls.text_node, ls.insert_node, ls.snippet_node
+			local c_type = {
+				s("cheadercomment", {
+					t("// Lloyd Williams (z5599988) | Written on " .. os.date("%d/%m/%Y ")),
+					t({ "", "// Description: " }),
+				}),
+				s("bigcomment", {
+					t("// " .. string.rep("=", 20) .. " "),
+					i(1),
+					t(" " .. string.rep("=", 20) .. " //"),
+				}),
+			}
+			ls.add_snippets("c", c_type)
+			ls.add_snippets("cpp", c_type)
+			ls.add_snippets("java", { c_type[2] })
 
-			local header = s("cheadercomment", {
-				t("// Lloyd Williams (z5599988) | " .. os.date("%d/%m/%Y ")),
-				t({ "", "// Description: " }),
-			})
-			local bigc = s("bigcomment", {
-				t("// " .. string.rep("=", 20) .. " "),
-				i(1),
-				t(" " .. string.rep("=", 20) .. " //"),
-			})
-			ls.add_snippets("c", { header, bigc })
-			ls.add_snippets("cpp", { header, bigc })
-			ls.add_snippets("java", { bigc })
+			-- snippet keymaps
+			vim.keymap.set("i", "<C-K>", function()
+				ls.expand()
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-L>", function()
+				ls.jump(1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-J>", function()
+				ls.jump(-1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-E>", function()
+				if ls.choice_active() then
+					ls.change_choice(1)
+				end
+			end, { silent = true })
 
-			local in_mathzone = function()
-				return vim.fn["vimtex#syntax#in_mathzone"]() == 1
-			end
-			ls.add_snippets("tex", {
-				s(
-					{ trig = "bf", snippetType = "autosnippet" },
-					d(1, function()
-						if in_mathzone() then
-							return sn(nil, { t("\\mathbf ") })
-						else
-							return sn(nil, { t("\\textbf ") })
-						end
-					end)
-				),
-				s({ trig = "bb", snippetType = "autosnippet" }, { t("\\mathbb ") }),
-			})
+			-- COQ + cmp setup
+			local coq = require("coq")
+			local cmp = require("cmp")
+			cmp.setup(coq.lsp_ensure_capabilities({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = coq.defaults(), -- use COQ’s recommended mappings
+				sources = cmp.config.sources({ -- mirror your source order
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				formatting = {
+					format = require("lspkind").cmp_format({ -- icons + text
+						mode = "symbol_text",
+						maxwidth = 35,
+						menu = {
+							buffer = "[Buffer]",
+							nvim_lsp = "[LSP]",
+							luasnip = "[LuaSnip]",
+							nvim_lua = "[Lua]",
+							latex_symbols = "[Latex]",
+							path = "[Path]",
+						},
+					}),
+				},
+			}))
 		end,
 	},
 }
