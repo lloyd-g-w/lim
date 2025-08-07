3154ed8704fc1d5928eba9a88c969ed6e5675fff
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 15:03:09 +1000

    🙃

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -101,74 +101,81 @@
 --   },
 -- }
 
-return {
-	{
-		"ms-jpq/coq_nvim",
-		lazy = false,
-		dependencies = {
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-			{ "ms-jpq/coq.thirdparty", branch = "3p" },
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"iurimateus/luasnip-latex-snippets.nvim",
-		},
-		init = function()
-			local cp = require("catppuccin.palettes").get_palette("frappe")
+return return {
+  {
+    "ms-jpq/coq_nvim",
+    lazy = false,
+    dependencies = {
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+      { "ms-jpq/coq.thirdparty", branch = "3p" },
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "iurimateus/luasnip-latex-snippets.nvim",
+    },
+    config = function()
+      -- COQ setup
+      require("coq").setup {
+        auto_start = "shut-up",
+        clients = {
+          lsp      = { enabled = true, min_chars = 1 },
+          buffer   = { enabled = true, min_chars = 1 },
+          path     = { enabled = true, min_chars = 1 },
+          snippets = { enabled = true, engine = "luasnip", min_chars = 1 },
+        },
+        keymap = {
+          eval_snips    = "<C-K>",
+          -- only supported keymap entries:
+          bigger_preview               = nil,
+          jump_to_mark                 = nil,
+          manual_complete              = nil,
+          manual_complete_insertion_only = false,
+          pre_select                   = false,
+          recommended                  = true,
+          repeat                       = nil,
+        },
+      }
 
-			vim.g.coq_settings = {
-				auto_start = "shut-up",
-				clients = {
-					lsp = { enabled = true, min_chars = 1 },
-					buffer = { enabled = true, min_chars = 1 },
-					path = { enabled = true, min_chars = 1 },
-					snippets = { enabled = true, engine = "luasnip", min_chars = 1 },
-				},
-				keymap = {
-					eval_snips = "<C-K>",
-					prev = "<S-Tab>",
-					next = "<Tab>",
-					select = "<CR>",
-				},
-			}
-		end,
-		config = function()
-			-- Snippet loading
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").load()
+      -- Snippet loading
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_lua").load()
 
-			-- Your custom snippets (unchanged)
-			local ls = require("luasnip")
-			local s, t, i, d, sn = ls.snippet, ls.text_node, ls.insert_node, ls.dynamic_node, ls.snippet_node
+      -- Custom snippets
+      local ls = require("luasnip")
+      local s, t, i, d, sn = ls.snippet, ls.text_node, ls.insert_node, ls.dynamic_node, ls.snippet_node
 
-			local header = s("cheadercomment", {
-				t("// Lloyd Williams (z5599988) | " .. os.date("%d/%m/%Y ")),
-				t({ "", "// Description: " }),
-			})
-			local bigc = s("bigcomment", {
-				t("// " .. string.rep("=", 20) .. " "),
-				i(1),
-				t(" " .. string.rep("=", 20) .. " //"),
-			})
-			ls.add_snippets("c", { header, bigc })
-			ls.add_snippets("cpp", { header, bigc })
-			ls.add_snippets("java", { bigc })
+      -- Header comment snippet
+      local header = s("cheadercomment", {
+        t("// Lloyd Williams (z5599988) | " .. os.date("%d/%m/%Y ")),
+        t({ "", "// Description: " }),
+      })
+      -- Big comment snippet
+      local bigc = s("bigcomment", {
+        t("// " .. string.rep("=", 20) .. " "),
+        i(1),
+        t(" " .. string.rep("=", 20) .. " //"),
+      })
+      ls.add_snippets("c", { header, bigc })
+      ls.add_snippets("cpp", { header, bigc })
+      ls.add_snippets("java", { bigc })
 
-			local in_mathzone = function()
-				return vim.fn["vimtex#syntax#in_mathzone"]() == 1
-			end
-			ls.add_snippets("tex", {
-				s(
-					{ trig = "bf", snippetType = "autosnippet" },
-					d(1, function()
-						if in_mathzone() then
-							return sn(nil, { t("\\mathbf ") })
-						else
-							return sn(nil, { t("\\textbf ") })
-						end
-					end)
-				),
-				s({ trig = "bb", snippetType = "autosnippet" }, { t("\\mathbb ") }),
-			})
-		end,
-	},
+      -- TeX snippets
+      local in_mathzone = function()
+        return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+      end
+      ls.add_snippets("tex", {
+        s(
+          { trig = "bf", snippetType = "autosnippet" },
+          d(1, function()
+            if in_mathzone() then
+              return sn(nil, { t("\\mathbf ") })
+            else
+              return sn(nil, { t("\\textbf ") })
+            end
+          end)
+        ),
+        s({ trig = "bb", snippetType = "autosnippet" }, { t("\\mathbb ") }),
+      })
+    end,
+  },
 }
+
