ebdeb9616a98bf2b3193131797904218bdbd0d7b
Lloyd <lloyd.williams1062@gmail.com>
2025-08-07 13:52:51 +1000

    🙃

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,34 +1,35 @@
 return {
-    'goolord/alpha-nvim',
-    config = function()
-        local alpha = require 'alpha'
-        local dashboard = require 'alpha.themes.dashboard'
-        --[[ dashboard.section.header.val = {
-            "                                   ",
-            "                                   ",
-            "                                   ",
-            "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-            "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-            "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-            "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-            "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-            "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-            "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-            " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-            " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-            "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-            "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-            "                                   ",
-        } --]]
-        dashboard.section.header.val = {
-"                                          ▄▄                   ",
-"▀███▄   ▀███▀                             ██                   ",
-"  ███▄    █                                                    ",
-"  █ ███   █   ▄▄█▀██  ▄██▀██▄▀██▀   ▀██▀▀███ ▀████████▄█████▄  ",
-"  █  ▀██▄ █  ▄█▀   ████▀   ▀██ ██   ▄█    ██   ██    ██    ██  ", 
-"  █   ▀██▄█  ██▀▀▀▀▀▀██     ██  ██ ▄█     ██   ██    ██    ██  ",
-"  █     ███  ██▄    ▄██▄   ▄██   ███      ██   ██    ██    ██  ", 
-"▄███▄    ██   ▀█████▀ ▀█████▀     █     ▄████▄████  ████  ████▄" }
-        alpha.setup(dashboard.opts)
-    end
-};
+	"goolord/alpha-nvim",
+	dependencies = { "catppuccin/nvim" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Catppuccin palette
+		local cp = require("catppuccin.palettes").get_palette("frappe")
+		-- Define a highlight group for our header
+		vim.api.nvim_set_hl(0, "AlphaHeader", { fg = cp.peach, bg = cp.base })
+
+		-- Cool Catppuccin-style ASCII art
+		dashboard.section.header.val = {
+			[[ ██╗     ██╗███╗   ███╗ ]],
+			[[ ██║     ██║████╗ ████║ ]],
+			[[ ██║     ██║██╔████╔██║ ]],
+			[[ ██║     ██║██║╚██╔╝██║ ]],
+			[[ ███████╗██║██║ ╚═╝ ██║ ]],
+			[[ ╚══════╝╚═╝╚═╝     ╚═╝ ]],
+		}
+		dashboard.section.header.opts.hl = "AlphaHeader"
+
+		-- Buttons
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", "<cmd>ene <BAR> startinsert<CR>"),
+			dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("p", "  Open project", "<cmd>Telescope projects<CR>"),
+			dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+		}
+
+		alpha.setup(dashboard.opts)
+	end,
+}
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -102,54 +102,42 @@
 -- }
 
 return {
-	-- Primary completion engine
 	{
 		"ms-jpq/coq_nvim",
-		branch = "coq",
+		build = ":COQdeps",
 		dependencies = {
-			-- Snippet engine support
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-			-- LSP completion source
-			{ "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
-			-- Latex symbols completion
-			{ "onsails/lspkind.nvim" },
-			-- Friendly snippets integration
-			{ "rafamadriz/friendly-snippets" },
-			-- LuaSnip loader
-			{ "L3MON4D3/LuaSnip" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "iurimateus/luasnip-latex-snippets.nvim" },
+			{ "ms-jpq/coq.artifacts", build = ":COQdeps" },
+			{ "ms-jpq/coq.thirdparty", module = "coq_3p" },
+			"onsails/lspkind.nvim",
+			"rafamadriz/friendly-snippets",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"iurimateus/luasnip-latex-snippets.nvim",
 		},
 		config = function()
-			-- Load VSCode-style snippets
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").load()
-
-			-- Coq setup
-			require("coq").setup({
+			-- Initialize COQ
+			local coq = require("coq")
+			coq.setup({
 				auto_start = true,
 				keymap = {
-					-- Navigate completion menu
 					eval_snips = "<C-K>",
 					prev = "<S-Tab>",
 					next = "<Tab>",
 					select = "<CR>",
 				},
 				clients = {
-					-- Enable LSP completion
 					lsp = { enabled = true },
-					-- Add buffer completion
 					buffer = { enabled = true },
-					-- Path completion
 					path = { enabled = true },
-					-- Snippet completions via LuaSnip
 					snippets = { enabled = true, engine = "luasnip" },
 				},
-				icons = {},
 			})
 
-			-- lspkind for symbol annotations
-			local lspkind = require("lspkind")
+			-- Load snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_lua").load()
+
+			-- lspkind integration
 			require("coq_3p").register({
 				lspkind = {
 					with_text = true,
@@ -164,25 +152,22 @@ return {
 				},
 			})
 
-			-- Custom C/CPP/Java snippets
+			-- Custom snippets (C/C++/Java/TeX)
 			local ls = require("luasnip")
 			local s, t, i, d, sn = ls.snippet, ls.text_node, ls.insert_node, ls.dynamic_node, ls.snippet_node
-			local c_type_snips = {
-				s("cheadercomment", {
-					t("// Lloyd Williams (z5599988) | Written on " .. os.date("%d/%m/%Y ")),
-					t({ "", "// Description: " }),
-				}),
-				s("bigcomment", {
-					t("// " .. string.rep("=", 20) .. " "),
-					i(1),
-					t(" " .. string.rep("=", 20) .. " //"),
-				}),
-			}
-			ls.add_snippets("c", c_type_snips)
-			ls.add_snippets("cpp", c_type_snips)
-			ls.add_snippets("java", { c_type_snips[2] })
+			local header = s("cheadercomment", {
+				t("// Lloyd Williams (z5599988) | Written on " .. os.date("%d/%m/%Y ")),
+				t({ "", "// Description: " }),
+			})
+			local bigc = s("bigcomment", {
+				t("// " .. string.rep("=", 20) .. " "),
+				i(1),
+				t(" " .. string.rep("=", 20) .. " //"),
+			})
+			ls.add_snippets("c", { header, bigc })
+			ls.add_snippets("cpp", { header, bigc })
+			ls.add_snippets("java", { bigc })
 
-			-- TeX snippets with mathzone detection
 			local in_mathzone = function()
 				return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 			end
