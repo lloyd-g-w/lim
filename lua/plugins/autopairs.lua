9c37d5b4e994c510e08e883491b498b2bd8a3861
Lloyd <lloyd.williams1062@gmail.com>
2025-10-21 17:34:01 +1100

    🙃

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,10 +2,4 @@ return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	opts = {}, -- this is equalent to setup({}) function
-	config = function()
-		local Rule = require("nvim-autopairs.rule")
-		local npairs = require("nvim-autopairs")
-
-		npairs.add_rule(Rule("`", "'", "tex"))
-	end,
 }
