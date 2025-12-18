1c475fdcfe1c46c2953025916302d0d4d7b6f133
Lloyd <lloyd.williams1062@gmail.com>
2025-12-18 16:28:31 +1100

    🙃

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,9 +1,22 @@
 return {
-	"catgoose/nvim-colorizer.lua",
-	event = "BufReadPre",
-	opts = {
-		user_default_options = {
-			css = true,
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = {
+			filetypes = { "*" },
+			user_default_options = {
+				RGB = true, -- #RGB hex
+				RGBA = true, -- #RGBA hex
+				RRGGBB = true, -- #RRGGBB
+				RRGGBBAA = true, -- #RRGGBBAA
+				AARRGGBB = false,
+				rgb_fn = true, -- rgb(...) support
+				hsl_fn = true, -- hsl(...) support
+				oklch_fn = true, -- **OKLCH support** (if plugin version includes this)
+				css = false,
+				css_fn = false,
+				mode = "background",
+			},
 		},
 	},
 }
