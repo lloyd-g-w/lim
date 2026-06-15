08e7a39bbea29e179ddefdf8d3d059c9ad9184c5
Lloyd <lloyd.williams1062@gmail.com>
2026-06-15 16:55:11 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/26.05";
   };
 
   outputs = inputs @ {
